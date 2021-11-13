c382e266d33dcf9407f0b3703391f5c7c2184882
Rahul P <rahulpadmaraju16@gmail.com>
2021-11-13 16:16:17 +0530

    ADD: commit to git

--- README.md
+++ README.md
@@ -7,10 +7,18 @@ Todolist application built on top ethereum blockchain network.
 ```md
 1. Install Ganache, Truffle Framework
 2. Start a local ethereum network on Ganache
-3. Install Dependencies - `npm install`
-4. Deploy the smart contract in this project - `truffle migrate --reset`
-5. Start the Lite Server - `npm run build`
+3. Install Dependencies - npm install
+4. Deploy the smart contract in this project - truffle migrate --reset
+5. Start the Lite Server - npm run build
 6. Install Metamask on your browser and point to the local Ganache network and add an account from the Ganache network
+```
+
+## Commands used
+
+```sh
+npm install
+truffle migrate --reset
+npm run build
 ```
 
 ## Tools used
---
+++ contracts/TodoList.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.5.0;
+
+contract TodoList {
+    uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("dummy task!");
+    }
+
+    event TaskCreated(uint id, string content, bool completed);
+
+    event TaskCompleted(uint id, bool completed);
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint _id) public {
+      Task memory _task = tasks[_id];
+      _task.completed = !_task.completed;
+      tasks[_id] = _task;
+      emit TaskCompleted(_id, _task.completed);
+    }
+}
